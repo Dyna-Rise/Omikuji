b764557e152404432edada64c7e76c5d4043eeaa
DESKTOP-C5T1MTL\user <dynarise.headquarters@gmail.com>
2025-08-25 13:19:33 +0900

    おみくじ 配列宣言

--- Omikuji/Program.cs
+++ Omikuji/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//おみくじ
+string[] omikuji = new string[5];
+omikuji[0] = "0大吉";
+omikuji[1] = "1中吉";
+omikuji[2] = "2吉";
+omikuji[3] = "3凶";
+omikuji[4] = "4大凶";
+
+//string[] omikuji = { "0大吉", "1中吉", "2吉", "3凶", "4大凶" };
