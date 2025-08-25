963e9481fe39f8534868da65cc740869e835b5fe
DESKTOP-C5T1MTL\user <dynarise.headquarters@gmail.com>
2025-08-25 13:36:29 +0900

    おみくじランダム1回まで

--- Omikuji/Program.cs
+++ Omikuji/Program.cs
@@ -7,3 +7,13 @@ omikuji[3] = "3凶";
 omikuji[4] = "4大凶";
 
 //string[] omikuji = { "0大吉", "1中吉", "2吉", "3凶", "4大凶" };
+
+//各おみくじを何回ひいたかを格納する配列
+int[] count = new int[5];
+
+//Randomクラスを実体化して変数randが機能を参照できる
+Random rand = new Random();
+int result = rand.Next(0,5); //0以上5未満のランダムを取得して変数resultに代入
+
+//result番目の値を配列からひっぱってきてコンソールに出す
+Console.WriteLine(omikuji[result]);
